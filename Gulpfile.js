c462d2a9949426112f99ab343bd230806622ed63
Vasiliy Gorokhov <vasiliy.gorokhov@omnigon.com>
2017-02-20 01:35:51 +0300

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+let gulp = require('gulp');
+let eslint = require('gulp-eslint');
+
+gulp.task('lint', () => {
+    return gulp.src([
+        '*.js',
+        '!node_modules/**'
+    ])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('pre-commit', ['lint']);
---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'spa-fe',
+    template: `<h1>Start</h1>`,
+})
+
+export class AppComponent  { }
